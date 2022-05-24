57f9cfda87be30815b584a70db288076baa91067
bale_marcu <balemarcu23@gmail.com>
2022-05-24 04:00:42 +0300

    teste Marcu

---
+++ src/test/java/com/academic/ISSProject/controller/EnrollControllerTest.java
@@ -0,0 +1,32 @@
+package com.academic.ISSProject.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EnrollControllerTest {
+
+    @Test
+    void getAllSpecializations() {
+    }
+
+    @Test
+    void getSpecialization() {
+    }
+
+    @Test
+    void getCurriculumOfSpecialization() {
+    }
+
+    @Test
+    void getCoursesOfCurriculum() {
+    }
+
+    @Test
+    void checkIfEnrolled() {
+    }
+
+    @Test
+    void enrollToSpecialization() {
+    }
+}
