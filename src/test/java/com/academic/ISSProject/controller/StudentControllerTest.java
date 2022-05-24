62fb662ac44a57943505728f1a1853b256814ff8
bale_marcu <balemarcu23@gmail.com>
2022-05-24 04:02:03 +0300

    teste Marcu

---
+++ src/test/java/com/academic/ISSProject/controller/StudentControllerTest.java
@@ -0,0 +1,36 @@
+package com.academic.ISSProject.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentControllerTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getStudent() {
+    }
+
+    @Test
+    void addStudent() {
+    }
+
+    @Test
+    void getCurrentUser() {
+    }
+
+    @Test
+    void updateProfile() {
+    }
+
+    @Test
+    void deleteStudent() {
+    }
+
+    @Test
+    void getGradesForStudent() {
+    }
+}
